46a2f74447ce198601d821837c5de1b4b364036b
mossaab <sgtmusabu@gmail.com>
2026-02-01 20:37:17 +0100

    working shit

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 
 
 
-
 const btn = document.getElementById("btn");
-
 // document.getElementById("mark").addEventListener("input", function () {
 //     this.value = this.value.replace(/[^0-9]/g, "");
 // });
@@ -16,9 +14,47 @@ document.querySelectorAll(".mark").forEach((markInput)=> {
 
 const input = document.getElementById("mark");
 
+
 btn.addEventListener('click', () => {
-    document.querySelectorAll(".mark").forEach((markInput)=> {
-    if (markInput.value)
-        console.log(markInput.value);
-})
+    let semAverage = 0;
+
+    document.querySelectorAll(".modules").forEach((module)=> {
+        module.querySelector(".moduleAverage").innerHTML = null;
+    });
+
+    document.querySelectorAll(".modules").forEach((module)=> {
+        if (module.id == "main") {
+            semAverage += calcModuleAverage(module, 3);
+        } else if (module.id == "secondary") {
+            semAverage += calcModuleAverage(module, 2)
+        } else {
+            semAverage += calcModuleAverage(module, 1);
+        }
+            
+    });
+
+    console.log(`Semester average ${(semAverage / 18).toFixed(2)}`);
 });
+
+
+function calcModuleAverage(module, coeff) {
+    if (module.querySelector("[name='TD']") && module.querySelector("[name='TP']")) {
+        let mAverage = Number(module.querySelector('[name="C"]').value) * 0.6 + (Number(module.querySelector('[name="TD"]').value) + Number(module.querySelector('[name="TP"]').value)) * 0.2;
+        module.querySelector(".moduleAverage").innerHTML += mAverage.toFixed(2);
+        return mAverage * coeff;
+    } else if (module.querySelector("[name='TD']") && !module.querySelector("[name='TP']")) {
+
+        let mAverage = Number(module.querySelector('[name="C"]').value) * 0.6 + Number(module.querySelector('[name="TD"]').value) * 0.4;
+        module.querySelector(".moduleAverage").innerHTML += mAverage.toFixed(2);
+        return mAverage * coeff;
+
+    } else if (module.querySelector("[name='TP']") && !module.querySelector("[name='TD']")) {
+        let mAverage = Number(module.querySelector('[name="C"]').value) * 0.6 + Number(module.querySelector('[name="TP"]').value) * 0.4;
+        module.querySelector(".moduleAverage").innerHTML += mAverage.toFixed(2);
+        return mAverage * coeff;
+    } else {
+        let mAverage = Number(module.querySelector('[name="C"]').value)
+        module.querySelector(".moduleAverage").innerHTML = mAverage.toFixed(2);        
+        return mAverage * coeff;
+    }
+}
